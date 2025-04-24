d492cac945dd975d0765c511800e7c1fccaf2b38
Muhammad Haroon Talib <2520028+tanzeelrana@users.noreply.github.com>
2025-04-24 15:07:24 -0400

    loader logo's and background changed

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -6917,7 +6917,7 @@ var _gsScope = "undefined" != typeof module && module.exports && "undefined" != 
             fauxPr: 0,
             fauxClipped: !1,
             init: function(t) {
-                this.el = document.getElementById("preloader"), this.inner = document.getElementsByClassName("js-loader-inner")[0], this.barWrapper = document.getElementsByClassName("js-bar-wrapper")[0], this.barProgress = document.getElementsByClassName("js-bar-progress")[0], this.drop = document.getElementsByClassName("js-drop")[0], this.cnv = document.createElement("canvas"), this.ctx = this.cnv.getContext("2d"), this.cnvDrop = document.createElement("canvas"), this.ctxDrop = this.cnvDrop.getContext("2d"), this.dropImg = new Image, this.dropImg.src = "devflovv_logo.ico", this.el.appendChild(this.cnv), this.onResize = this.onResize.bind(this), this.onResize(), this.addEvents(), this.setDefault(), this.start()
+                this.el = document.getElementById("preloader"), this.inner = document.getElementsByClassName("js-loader-inner")[0], this.barWrapper = document.getElementsByClassName("js-bar-wrapper")[0], this.barProgress = document.getElementsByClassName("js-bar-progress")[0], this.drop = document.getElementsByClassName("js-drop")[0], this.cnv = document.createElement("canvas"), this.ctx = this.cnv.getContext("2d"), this.cnvDrop = document.createElement("canvas"), this.ctxDrop = this.cnvDrop.getContext("2d"), this.dropImg = new Image, this.dropImg.src = "DF-Mono.png", this.el.appendChild(this.cnv), this.onResize = this.onResize.bind(this), this.onResize(), this.addEvents(), this.setDefault(), this.start()
             },
             addEvents: function() {
                 window.addEventListener("resize", this.onResize)
@@ -7028,7 +7028,7 @@ var _gsScope = "undefined" != typeof module && module.exports && "undefined" != 
                 var t = this.pixelRatio = window.devicePixelRatio;
                 this.w = window.innerWidth, this.h = window.innerHeight, this.cnv.width = t * this.w, this.cnv.height = t * this.h, this.cnv.style.width = this.w + "px", this.cnv.style.height = this.h + "px", this.ctx.scale(t, t), this.innerW = Math.min(.6 * this.w, 500), this.innerH = .1 * this.h, this.inner.style.left = Math.round(.5 * (this.w - this.innerW)) + "px", this.inner.style.top = Math.round(.5 * (this.h - this.innerH)) + "px", this.inner.style.width = this.innerW + "px", this.inner.style.height = this.innerH + "px";
                 var e = n.MOBILE ? .56 : .8;
-                this.dropW = 50 * e, this.dropH = 48 * e, this.cnvDrop.width = Math.ceil(t * this.dropW), this.cnvDrop.height = Math.ceil(t * this.dropH), this.ctxDrop.scale(t, t)
+                this.dropW = 90 * e, this.dropH = 88 * e, this.cnvDrop.width = Math.ceil(t * this.dropW), this.cnvDrop.height = Math.ceil(t * this.dropH), this.ctxDrop.scale(t, t)
             }
         })
     }), require.config({
